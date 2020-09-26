8a624daeab44a8083b8e8f9178eaa5044db59138
LearNAV <thenavigator.app15@gmail.com>
2020-09-26 22:22:27 +0800

    Integrated Interpolation of Strings and Using Variables from another Class

--- Libre_Update/Libre_Update/Libre Engine/CheckDatabase.cs
+++ Libre_Update/Libre_Update/Libre Engine/CheckDatabase.cs
@@ -20,8 +20,8 @@ namespace Libre_Update.Libre_Engine
         /// **Connection String**
         /// Also Includes all needed connection properties
         /// </summary>
-		static string connectionString = "DataSource=" + Environment.CurrentDirectory + "\\LEARNAV.DB;Version=3";
-		static SQLiteConnection dbCn = new SQLiteConnection(connectionString);
+		
+		static SQLiteConnection dbCn = new SQLiteConnection(Libre_Engine.VarHold.connectionString);
 		static SQLiteDataAdapter da = new SQLiteDataAdapter();
 
         /// <summary>
@@ -59,12 +59,15 @@ namespace Libre_Update.Libre_Engine
         /// </summary>
 		public static void InitialLoad()
 		{
+            private string connectionStringSelect = Libre_Engine.VarHold.connectionString;
+            private string tableName = Libre_Engine.VarHold.tableNames[0];
+
+            string selectQuery = ($"SELECT * FROM {tableName}");
+            SQLiteCommand sqlInitialLoadCommand = new SQLiteCommand(selectQuery, dbCn);
 			try 
 			{
 			dbCn.Open();
-            string selectQuery = ($"SELECT * FROM {tableNames[0]}")
-            SQLiteCommand sqlInitialLoadCommand = new SQLiteCommand()
-            da = new SQLiteDataAdapter($"SELECT * FROM ResourceDB", dbCn);
+            da = new SQLiteDataAdapter(sqlInitialLoadCommand);
 			da.Fill(dt);
 			dbCn.Close();
 			} catch (System.Exception e) 
@@ -72,10 +75,7 @@ namespace Libre_Update.Libre_Engine
 					message = ("Error With Initial Load! \n Message: " + e.Message);
 			}
 		}
-        }
-
-		
-
+        }	
     }
     /// <summary>
     /// Advanced Search Class
@@ -84,8 +84,9 @@ namespace Libre_Update.Libre_Engine
     /// </summary>
     public static class AdvanceSearch
     {
-    	static string connection_string = "DataSource=" + Environment.CurrentDirectory + "\\LEARNAV.DB;Version=3";
-		static SQLiteConnection db_cn = new SQLiteConnection(connection_string);
+    	
+        private string connectionString = Libre_Engine.VarHold.connectionString;
+		static SQLiteConnection db_cn = new SQLiteConnection(connectionString);
 		static SQLiteDataAdapter da = new SQLiteDataAdapter();
 		public static DataTable dt = new DataTable();
         /// <summary>
@@ -104,15 +105,22 @@ namespace Libre_Update.Libre_Engine
         /// </summary>
         public static void searchName(string _searchName)
         {
+            private string connectionStringSearchName = Libre_Engine.VarHold.connectionString;
+            private string tableNameSearchName = Libre_Engine.VarHold.tableNames[0];
 
-            
-        
-        	dbCn.Open();
-        	da = new SQLiteDataAdapter("SELECT * FROM ResourceDB WHERE ResourceN like '%" + _searchName + "%'" , db_cn);
+            string selectQuery = ($"SELECT * FROM {tableNameSearchName}");
+            SQLiteCommand sqlInitialLoadCommand = new SQLiteCommand(selectQuery, dbCn);
+            try 
+            {
+              dbCn.Open();
+            da = new SQLiteDataAdapter("SELECT * FROM ResourceDB WHERE ResourceN like '%" + _searchName + "%'" , db_cn);
             dt.Clear();
-        	da.Fill(dt);
-        	DataView dv = new DataView(dt);
-           // dv.RowFilter = string.Format ("ID LIKE")
+            da.Fill(dt);
+            DataView dv = new DataView(dt);  
+            } catch {System.Exception ae) 
+            {
+                
+            } 
 
         }
 
