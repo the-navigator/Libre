daee01bddf0c5885bf55cfe8f50dae3828ce0964
LearNAV <thenavigator.app15@gmail.com>
2020-09-27 22:25:25 +0800

    Finsihed basic structure of FileObject with complete constructors

--- Libre_Update/Libre_Update/Libre Engine/FileAttachmentModule.cs
+++ Libre_Update/Libre_Update/Libre Engine/FileAttachmentModule.cs
@@ -9,18 +9,47 @@ using System.IO;
 
 namespace Libre_Update.Libre_Engine
 {
+	/// <summary>
+	/// Stores all data to be stored in JSON file
+	/// </summary>
+	public class DataHolderClass
+    {
+      private List<FileObject> allObj = new List<FileObject>;
+    }
+
+    /// <summary>
+    /// A File Object
+    /// Stores Data of a FileObject to be stored in a JSON file
+    /// Data includes Name, Extension, DateCreated and Special Characters
+    /// </summary>
     public class FileObject
     {
-    	public static string fileObjName;
-    	public static string fileObjExtension;
-    	public static string fileObjDateCreated;
-    	public static string fileObjSpecialCharacters;
 
+    	public static string fileObjName {get;}
+    	public static string fileObjExtension {get;}
+    	public static string fileObjDateCreated {get;}
+    	public static string fileObjSpecialCharacters {get;}
+
+    	public static FileInfo fileInfoOfSelectedObj = new FileInfo(fileObjName);
+
+    	/// <summary>
+    	/// Constructor Used to define this Object
+    	/// </summary>
+    	public FileObject(string _fileObjName, string _fileObjExtension, string _fileObjDateCreated, string _fileObjSpecialCharacters)
+    	{
+    		this.fileObjName = _fileObjName;
+    		this.fileObjExtension = _fileObjExtension;
+    		this.fileObjDateCreated = _fileObjDateCreated;
+    		this.fileObjSpecialCharacters = _fileObjSpecialCharacters;
+    	}
+
+    	public void AddFileAsObj (string filePath)
+    	{
+    		fileObjName = filePath;
+
+    	}
 
     }
 
-    public class ClassName 
-    {
-      
-    }
+   
 }
